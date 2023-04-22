4ee44b6f7fe5ec0e85c816fc31d470dea7231983
Riya Agarwal <53482709+riyagarwal@users.noreply.github.com>
2023-04-22 11:30:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+const addTodo = () => {
+	const todoItem = document.getElementById('newTodoInput').value;
+	if(todoItem) {
+		let liItem = document.createElement('li');
+		liItem.textContent = 'todoItem';
+		document.getElementById('todoList').appendChild(liItem)
 
+		document.getElementById('newTodoInput').value('')
+	}
+}
